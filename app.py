be75fae157eb3ad16142eea29ec468e53f52df52
SIR.GEO <g.miller@millerlog.net>
2024-01-19 16:38:38 +0000

    updated ai prompt and temperature

--- app.py
+++ app.py
@@ -84,9 +84,10 @@ if search_query:
     try:
         search_response = client.chat.completions.create(
             model="gpt-4-1106-preview",
+            temperature=0.5
             stream = True,
             messages=[
-                {"role": "system", "content": "You are a professional analyst called OAT Docs Analyser assistant. You must say if the information does not have enough detail, you must NOT make up facts or lie. At the end of any response, you must always source any information you used in your response, each document source will be given in the format *Document Source: (content filename here)*. You must always answer the user's questions using the context given:" + all_file_contents},
+                {"role": "system", "content": "You are a professional analyst called OAT Docs Analyser assistant. You must say if the information does not have enough detail, you must NOT make up facts or lie. At the end of any response, you must always source any information you used in your response, each document source will be given in the format **Document Source: (content filename here)**. You must always answer the user's questions using the context given:" + all_file_contents},
                 {"role": "user", "content": search_query}
             ]
         )
