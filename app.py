737ad0d4ada364be9fb4957ac7c61a8438643eef
SIR.GEO <g.miller@millerlog.net>
2024-01-19 02:55:21 +0000

    added api get in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from config import OPENAI_API_KEY, PINECONE_API_KEY, PINECONE_API_ENVIRONMENT
+# from config import OPENAI_API_KEY, PINECONE_API_KEY, PINECONE_API_ENVIRONMENT
 from constants import PINECONE_INDEX_NAME
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Pinecone
@@ -8,12 +8,10 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 import pinecone
 
-# Set API key for OpenAI and Pinecone
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-pinecone.init(
-    api_key=PINECONE_API_KEY,
-    environment=PINECONE_API_ENVIRONMENT
-)
+# Use os.environ.get to access environment variables
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+PINECONE_API_ENVIRONMENT = os.environ.get("PINECONE_API_ENVIRONMENT")
 
 # Setting streamlit
 st.title('OAT Policy Analyser')
