222f5a7083a5dafa85cad2f34ddf4c422879e6a6
SIR.GEO <g.miller@millerlog.net>
2024-01-19 03:06:59 +0000

    added index constant

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pinecone
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 PINECONE_API_ENVIRONMENT = os.environ.get("PINECONE_API_ENVIRONMENT")
+PINECONE_INDEX_NAME = 'oat'
 
 # Setting streamlit
 st.title('OAT Policy Analyser')
