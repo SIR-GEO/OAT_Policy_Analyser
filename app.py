13c611a621065b342dc78901a76ae22ca1592796
SIR.GEO <g.miller@millerlog.net>
2024-01-21 16:51:30 +0000

    update

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if password == st.secrets["general"]["password"]:
 
     # Add a slider for the temperature
 
-    temperature = st.slider('Percentage Creativity Slider | 0.0 = Highly Predictable | 1.0 = Super Creative', min_value=0.0, max_value=1.0, value=0.5, step=0.1, format='%.1f')
+    temperature = st.slider('Creativity Slider | 0.0 = Highly Predictable | 1.0 = Super Creative', min_value=0.0, max_value=1.0, value=0.5, step=0.1, format='%.1f')
 
 
     # At the top of your Streamlit app, after imports
